769d388d0ff8205f9e70c25b4a2ef9650f1d0eb6
ここãApple <blackapple.0214@gmail.com>
2020-10-15 17:31:16 +0900

    recipe controller setting

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,13 +1,45 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!, only: [:show, :create]
   def index
+    search_word = params["recipe"]
+  	@recipes = Recipe.where(['title LIKE ?', "%#{search_word}%"]) #検索に関する投稿一覧表示
+    @recipe = Recipe.new #新規投稿できるように
+    @all_ranks = Recipe.find(Like.group(:recipe_id).order('count(recipe_id) desc').limit(3).pluck(:recipe_id))
   end
-
-  def new
-  end
-
   def show
+  	@recipe = Recipe.find(params[:id])
+	  @recipes = Recipe.where(['title LIKE ?', "%#{@recipe.title}%"])
+    @comments = @recipe.comments.order(id: "DESC") #投稿詳細に関連つけてあるコメントの取得
+    @comment = Comment.new #新規コメントできるように
+    #新しいコメント順にした
   end
-
+  def new
+  	@recipe = Recipe.new
+    @genres = Genre.all
+  end
+  def create
+  	@recipe = Recipe.new(recipes_params)
+    #すでにあるgenreは選択できるようにし、ない物は新規追加させて、保存できる
+    genre = Genre.find_or_create_by(name: params[:recipe][:genre_id])
+    @recipe.user_id = current_user.id
+    @recipe.genre_id = genre.id
+    if @recipe.save
+      redirect_to recipes_path
+    else
+      redirect_to request.referer
+    end
+  end
+  def update
+    @comment = Comment.find_by(id:params[:id])
+    @comment.content = params[:content]
+    @comment.save
+    redirect_to("/recipes/index")
+  end
   def edit
+    @recipe = Recipe.find_by(id: params[:id])
+  end
+private
+  def recipes_params
+  	params.require(:recipe).permit(:title, :photo, :cooking, :item)
   end
 end
